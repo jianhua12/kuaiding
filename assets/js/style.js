731f2311ecc1ec21e7424ba4efb83ce5c38ec51a
jianhua12 <1161338679@qq.com>
2018-01-02 13:03:37 +0800

    a
    Signed-off-by: jianhua12 <1161338679@qq.com>

---
+++ assets/js/style.js
@@ -0,0 +1,7 @@
+$(function(){
+	$('.nav-list .active a').mouseover(function(){
+		$(this).addClass('.sidebar_li')
+	})
+	$('.nav-list .active a').mouseout(function(){
+		$(this).addClass('.sidebar_li')
+})
